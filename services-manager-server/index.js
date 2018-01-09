ea7be9e2d02b37d995f7ab31059fc9a12d042ede
Dawn Nowell <dawn@bleatport.com>
2018-01-08 16:42:09 -0800

    Updated README.md to highlight existence of sms

--- services-manager-server/index.js
+++ services-manager-server/index.js
@@ -1,5 +1,15 @@
 const Express = require('express');
 const ServicesManagerServer = Express();
+var Shell = require('shelljs');
+
+if (!shell.which('bash'))
+{
+	shell.echo('Even if you dont use arch linux, bash is a minimum requuirement for this addon.');
+
+	shell.exit(1);
+}
+
+var ShellProcess = exec('sh ~/services-server-manager/sh/sonarr.sh')
 
 ServicesManagerServer.get('/sonarr/restart', (request, response) => response.send('Restart Sonarr!'));
 ServicesManagerServer.get('/radarr/restart', (request, response) => response.send('Restart Radarr!'));
